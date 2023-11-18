249224fd2a58708174bffb0208ae4c9cf211959e
VHriho <v12grig12@gmail.com>
2023-11-18 03:11:16 +0200

    added dimension method

--- src/main/java/Matrix.java
+++ src/main/java/Matrix.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Random;
 
 public class Matrix implements IMatrix {
@@ -98,6 +99,21 @@ public class Matrix implements IMatrix {
             }
             return column;
         }
+    }
+
+    //повертає розмірність матриці
+    @Override
+    public HashMap<String, Integer> getDimension() {
+        HashMap<String, Integer> map = new HashMap<>();
+        if (elements.length == 0 || elements[0].length == 0) {
+            map.put("Rows", 0);
+            map.put("Columns", 0);
+        }
+        else {
+            map.put("Rows", elements.length);
+            map.put("Columns", elements[0].length);
+        }
+        return map;
     }
 
     //перевизначений метод equals
